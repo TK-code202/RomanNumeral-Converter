301a4a356550d214fb4a9861d0bbcc51eebe375e
Tofunmi Kusimo <tofunmikusimo21@gmail.com>
2023-06-22 12:48:24 +0100

    add business logic to script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,21 @@
-// function integerToRoman(num) {
-//     if (typeof num !== 'number') {
-//         return false;
-//     }
-//     var digits = String(+num).split(""),
-//         key = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "", "I", "II", "III", "IV", "VI", "VII", "VIII", "IX"],
-//         roman_num = "",
-//         i = 3;
-//     while (i--)
-//         roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num; 
-// return Array(+digits.join("")+1).join("M") + roman_num;
-// }
-
+//Business Logic
 
 function convertToRoman (num) {
     if (num <= 0|| num > 3999) {
         return "<p>Invalid! Enter a valid number</p>" ;
     }
-    const numeralCodes = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], ["", "C", "CC", "CCC" , "CD", "D", "DC", "DCC", "DCCC", "CM"],  ["", "M", "MM", "MMM"]];
+    const numeralValues = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], ["", "C", "CC", "CCC" , "CD", "D", "DC", "DCC", "DCCC", "CM"],  ["", "M", "MM", "MMM"]];
 
     let numeral = "";
     const digits = num.split("").reverse();
     for (let i = 0; i < digits.length; i++) {
-        numeral  = numeralCodes[i][parseInt(digits[i])] + numeral;
+        numeral  = numeralValues[i][parseInt(digits[i])] + numeral;
     }
 
     return numeral;
 }
 
-//UI Logic
+
 $(document).ready(function() {
     $("#formOne").submit(function(event) {
         const inputtedNumber = $("#Number").val();
